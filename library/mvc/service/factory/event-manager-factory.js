97e7c2733fb05ae7922df8222081e98ebea13ab3
Mizanur Rahman (Zaizi) <mrahman@zaizi.com>
2026-03-02 13:59:27 +0000

    Remove library to re-add as subtree

--- library/mvc/service/factory/event-manager-factory.js
+++
@@ -1,8 +0,0 @@
-// library/mvc/service/factory/event-manager-factory.js
-const EventManager = require('../../../event-manager/event-manager');
-
-module.exports = class EventManagerFactory {
-  createService(serviceManager) {
-    return new EventManager();
-  }
-};
